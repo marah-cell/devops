b24f63dba30235a7b7e33171db8bc954c0af2d5c
marah <memo@DESKTOP-0VBEHH5>
2025-05-10 13:06:44 +0300

    devops

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+const mongoURI = process.env.MONGO_URI || 'mongodb://mongo:27017/mydatabase';
+
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ Connected to MongoDB'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from Node.js with MongoDB!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
